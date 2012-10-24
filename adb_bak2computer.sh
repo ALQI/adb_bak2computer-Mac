cf1c0421da5c60b019e8df93ac9f10317493fc66
ALQI <therealalquimista@gmail.com>
2012-10-23 18:00:39 -0700

    Base Script and Cleanup
    The final working functions are now in, but not called.  This will be
    what I build the ".command" files off of.  Cleaned up a bit for
    pleastries.

--- adb_bak2computer.sh
+++ adb_bak2computer.sh
@@ -29,136 +29,208 @@
 # so I can save space on my external and internal sdcards cause stock touchwiz is HUGE
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-# Required variables
-LOCALPATH="/path/to/store/the/backup"
+
+## STORAGE: This data is for future use ##
+#LOCALPATH="/path/to/store/the/backup"
 
 # Standard blocks
-SYS_BLK="/dev/block/mmcblk0p14"
-DAT_BLK="/dev/block/mmcblk0p15"
-BOT_BLK="/dev/block/mmcblk0p7"
-
+# SYS_BLK="/dev/block/mmcblk0p14"
+# DAT_BLK="/dev/block/mmcblk0p15"
+# BOT_BLK="/dev/block/mmcblk0p7"
 
 # Optional blocks
-CHE_BLK="/dev/block/mmcblk0p17"
-REC_BLK="/dev/block/mmcblk0p18"
-MOD_BLK="/dev/block/mmcblk0p1"
-LT1_BLK="/dev/block/mmcblk0p12"
-LT2_BLK="/dev/block/mmcblk0p13"
+# CHE_BLK="/dev/block/mmcblk0p17"
+# REC_BLK="/dev/block/mmcblk0p18"
+# MOD_BLK="/dev/block/mmcblk0p1"
+# LT1_BLK="/dev/block/mmcblk0p12"
+# LT2_BLK="/dev/block/mmcblk0p13"
+## END_STORAGE ##
 
-
-# backup system partition
+## SYSTEM: backup system partition ##
 system_bak1(){
 	adb forward tcp:5555 tcp:5555
-	adb shell
-	/system/xbin/busybox nc -l -p 5555 -e /system/xbin/busybox dd if=/dev/block/mmcblk0p14
+	echo "/system/xbin/busybox nc -l -p 5555 -e /system/xbin/busybox dd bs=4096 if=/dev/block/mmcblk0p14" | adb shell
 }
 
 system_bak2(){
 	adb forward tcp:5555 tcp:5555
-	cd $LOCALPATH
-	nc 127.0.0.1 5555 | pv -i 0.5 > /mmcblk0p14_system.img bs=4096
+	cd ./adb_bak2computer
+	nc 127.0.0.1 5555 | pv -i 0.5 > ./mmcblk0p14_system.img
 }
 
-# backup recovery partition
+system_bak0(){
+	cd ./adb_bak2computer
+	open ./system_bak1.command
+	sleep 2
+	open ./system_bak2.command
+	wait
+}
+
+## END_SYSTEM ##
+
+## RECOVERY: backup recovery partition ##
 recovery_bak1(){
 	adb forward tcp:5555 tcp:5555
-	adb shell
-	/system/xbin/busybox nc -l -p 5555 -e /system/xbin/busybox dd if=/dev/block/mmcblk0p18
+	echo "/system/xbin/busybox nc -l -p 5555 -e /system/xbin/busybox dd bs=4096 if=/dev/block/mmcblk0p18" | adb shell
 }
 
 recovery_bak2(){
 	adb forward tcp:5555 tcp:5555
-	cd $LOCALPATH
-	nc 127.0.0.1 5555 | pv -i 0.5 > /mmcblk0p18_recovery.img bs=4096
+	cd ./adb_bak2computer
+	nc 127.0.0.1 5555 | pv -i 0.5 > ./mmcblk0p18_recovery.img
 }
 
-# backup data partition
+recovery_bak0(){
+	cd ./adb_bak2computer
+	open ./recovery_bak1.command
+	sleep 2
+	open ./recovery_bak2.command
+	wait
+}
+
+## END_RECOVERY ##
+
+## DATA: backup data partition ##
 data_bak1(){
 	adb forward tcp:5555 tcp:5555
-	adb shell
-	/system/xbin/busybox nc -l -p 5555 -e /system/xbin/busybox dd if=/dev/block/mmcblk0p15
+	echo "/system/xbin/busybox nc -l -p 5555 -e /system/xbin/busybox dd bs=4096 if=/dev/block/mmcblk0p15" | adb shell
 }
 
 data_bak2(){
 	adb forward tcp:5555 tcp:5555
-	cd $LOCALPATH
-	nc 127.0.0.1 5555 | pv -i 0.5 > /mmcblk0p15_data.img bs=4096
+	cd ./adb_bak2computer
+	nc 127.0.0.1 5555 | pv -i 0.5 > ./mmcblk0p15_data.img
 }
 
-# backup CDMA MODEM
+data_bak0(){
+	cd ./adb_bak2computer
+	open ./data_bak1.command
+	sleep 2
+	open ./data_bak2.command
+	wait
+}
+## END_DATA ##
+
+## CDMA: backup cdma modem ##
 modem_cdma_bak1(){
 	adb forward tcp:5555 tcp:5555
-	adb shell
-	/system/xbin/busybox nc -l -p 5555 -e /system/xbin/busybox dd if=/dev/block/mmcblk0p1
+	echo "/system/xbin/busybox nc -l -p 5555 -e /system/xbin/busybox dd bs=4096 if=/dev/block/mmcblk0p1" | adb shell
 }
 
 modem_cdma_bak2(){
 	adb forward tcp:5555 tcp:5555
-	cd $LOCALPATH
-	nc 127.0.0.1 5555 | pv -i 0.5 > /mmcblk0p1_modem.img bs=4096
+	cd ./adb_bak2computer
+	nc 127.0.0.1 5555 | pv -i 0.5 > ./mmcblk0p1_modem.img
 }
 
-# backup LTE modem1
+modem_cdma_bak0(){
+	cd ./adb_bak2computer
+	open ./modem_cdma_bak1.command
+	sleep 2
+	open ./modem_cdma_bak2.command
+	wait
+}
+
+## END_CDMA ##
+
+## LTE1: backup LTE modem1 ##
 modem1_lte_bak1(){
 	adb forward tcp:5555 tcp:5555
-	adb shell
-	/system/xbin/busybox nc -l -p 5555 -e /system/xbin/busybox dd if=/dev/block/mmcblk0p12
+	echo "/system/xbin/busybox nc -l -p 5555 -e /system/xbin/busybox dd bs=4096 if=/dev/block/mmcblk0p12" | adb shell
 }
 
 modem1_lte_bak2(){
 	adb forward tcp:5555 tcp:5555
-	cd $LOCALPATH
-	nc 127.0.0.1 5555 | pv -i 0.5 > /mmcblk0p12_modemst1.img bs=4096
+	cd ./adb_bak2computer
+	nc 127.0.0.1 5555 | pv -i 0.5 > ./mmcblk0p12_modemst1.img
 }
 
-# backup LTE modem2
+modem1_lte_bak0(){
+	cd ./adb_bak2computer
+	open ./modem1_lte_bak1.command
+	sleep 2
+	open ./modem1_lte_bak2.command
+	wait	
+}
+
+## END_LTE1 ##
+
+## LTE2: backup LTE modem2 ##
 modem2_lte_bak1(){
 	adb forward tcp:5555 tcp:5555
-	adb shell
-	/system/xbin/busybox nc -l -p 5555 -e /system/xbin/busybox dd if=/dev/block/mmcblk0p13
+	echo "/system/xbin/busybox nc -l -p 5555 -e /system/xbin/busybox dd bs=4096 if=/dev/block/mmcblk0p13" | adb shell
 }
 
 modem2_lte_bak2(){
 	adb forward tcp:5555 tcp:5555
-	cd $LOCALPATH
-	nc 127.0.0.1 5555 | pv -i 0.5 > /mmcblk0p13_modemst1.img bs=4096
+	cd ./adb_bak2computer
+	nc 127.0.0.1 5555 | pv -i 0.5 > ./mmcblk0p13_modemst1.img
 }
 
-# backup cache
+modem2_lte_bak0(){
+	cd ./adb_bak2computer
+	open ./modem2_lte_bak1.command
+	sleep 2
+	open ./modem2_lte_bak2.command
+	wait
+}
+
+## END_LTE2 ##
+
+## CACHE: backup cache ##
 cache_bak1(){
 	adb forward tcp:5555 tcp:5555
-	adb shell
-	/system/xbin/busybox nc -l -p 5555 -e /system/xbin/busybox dd if=/dev/block/mmcblk0p17
+	echo "/system/xbin/busybox nc -l -p 5555 -e /system/xbin/busybox dd bs=4096 if=/dev/block/mmcblk0p17" | adb shell
 }
 
 cache_bak2(){
 	adb forward tcp:5555 tcp:5555
-	cd $LOCALPATH
-	nc 127.0.0.1 5555 | pv -i 0.5 > /mmcblk0p17_cache.img bs=4096
+	cd ./adb_bak2computer
+	nc 127.0.0.1 5555 | pv -i 0.5 > ./mmcblk0p17_cache.img
 }
 
-# backup boot
+cache_bak0(){
+	cd ./adb_bak2computer
+	open ./cache_bak1.command
+	sleep 2
+	open ./cache_bak2.command
+	wait
+}
+
+## END_CACHE ##
+
+## BOOT: backup boot ##
 boot_bak1(){
 	adb forward tcp:5555 tcp:5555
-	adb shell
-	/system/xbin/busybox nc -l -p 5555 -e /system/xbin/busybox dd if=/dev/block/mmcblk0p7
+	echo "/system/xbin/busybox nc -l -p 5555 -e /system/xbin/busybox dd bs=4096 if=/dev/block/mmcblk0p7" | adb shell
 }
 
 boot_bak2(){
 	adb forward tcp:5555 tcp:5555
-	cd $LOCALPATH
-	nc 127.0.0.1 5555 | pv -i 0.5 > /mmcblk0p7_boot.img bs=4096
+	cd ./adb_bak2computer
+	nc 127.0.0.1 5555 | pv -i 0.5 > /mmcblk0p7_boot.img
 }
 
-boot_bak0
+boot_bak0(){
+	cd ./adb_bak2computer
+	open ./boot_bak1.command
+	sleep 2
+	open ./boot_bak2.command
+	wait
+}
 
-# pick a partition menu
+## END_BOOT ##
+
+## MENU1: pick a partition menu ##
 menu1(){
 	# have to write the stupid menu
 }
 
+## END_MENU1 ##
+
+## MAIN: This is where the magic happens ##
 main(){
 	# um do really cool stuff
 }
 
-
+## END_MAIN ##
